fe1d53138d3d14cbd549136f6ef2b1748c979987
Pan, Xinyang <seanpan2008@gmail.com>
2017-05-16 12:09:52 +0800

    add toString

--- src/main/java/io/github/xinyangpan/codegen/Tools.java
+++ src/main/java/io/github/xinyangpan/codegen/Tools.java
@@ -5,6 +5,7 @@ import static org.apache.commons.lang3.StringUtils.capitalize;
 import java.util.LinkedHashSet;
 import java.util.List;
 
+import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.builder.Builder;
 import org.springframework.core.convert.converter.Converter;
 
@@ -12,6 +13,7 @@ import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 
 import io.github.xinyangpan.codegen.builder.BuilderGenerator;
+import io.github.xinyangpan.codegen.classfile.part.FieldPart;
 import io.github.xinyangpan.codegen.classfile.part.MethodPart;
 import io.github.xinyangpan.codegen.classfile.part.ParameterPart;
 import io.github.xinyangpan.codegen.classfile.type.ClassType;
@@ -21,6 +23,28 @@ import io.github.xinyangpan.codegen.converter.ConverterGenerator;
 import io.github.xinyangpan.commons.PropertyCollectingType;
 
 public class Tools {
+
+	public static MethodPart generateToString(List<FieldPart> fieldParts) {
+		List<String> contents = Lists.newArrayList();
+		if (CollectionUtils.isNotEmpty(fieldParts)) {
+			contents.add("StringBuilder builder = new StringBuilder();");
+			contents.add(String.format("builder.append(\"TblPerson [%s=\");", fieldParts.get(0).getName()));
+			contents.add(String.format("builder.append(%s);", fieldParts.get(0).getName()));
+			for (int i = 1; i < fieldParts.size(); i++) {
+				contents.add(String.format("builder.append(\", %s=\");", fieldParts.get(i).getName()));
+				contents.add(String.format("builder.append(%s);", fieldParts.get(i).getName()));
+			}
+			contents.add("builder.append(\"]\");");
+			contents.add("return builder.toString();");
+		} else {
+			contents.add("return super.toString();");
+		}
+		// 
+		MethodPart methodPart = new MethodPart("toString", ClassWrapper.of(String.class));
+		methodPart.setAnnotationWrapper(Lists.newArrayList(new AnnotationWrapper(Override.class)));
+		methodPart.setContents(contents);
+		return methodPart;
+	}
 
 	public static ClassType generateConverter(Class<?> source, Class<?> target, String packageName, String paramName) {
 		// 
